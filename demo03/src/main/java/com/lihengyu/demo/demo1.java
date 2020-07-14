a4e9774873147b240838ceab49d3b0ccc2b1be76
hengyu-li <3144046526@qq.com>
2020-07-14 21:49:54 +0800

    第一次

---
+++ demo03/src/main/java/com/lihengyu/demo/demo1.java
@@ -0,0 +1,15 @@
+package com.lihengyu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("启动注册中心3333");
+        SpringApplication.run(demo1.class,args);
+    }
+}
